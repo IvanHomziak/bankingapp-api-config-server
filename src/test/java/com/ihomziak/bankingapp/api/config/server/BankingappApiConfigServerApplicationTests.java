ef7a28b60c44ae6fe18506ec3ec12039fdb9c7f8
ivanhomziak <ivan.homziak@gmail.com>
2025-01-12 17:55:37 +0100

    Initial commit

---
+++ src/test/java/com/ihomziak/bankingapp/api/config/server/BankingappApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihomziak.bankingapp.api.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingappApiConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
